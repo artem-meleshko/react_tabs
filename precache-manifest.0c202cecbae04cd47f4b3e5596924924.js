4bf6815ecb92e69e44efe68b29305279754a8d7f
artem-meleshko <timonlondon@hotmail.com>
2019-07-30 14:24:28 +0300

    Updates

---
+++ precache-manifest.0c202cecbae04cd47f4b3e5596924924.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0190fe1da9530b6c9dd0f546aa57e902",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "1b6d236442143cfa38d3",
+    "url": "/react_tabs/static/css/main.b5384113.chunk.css"
+  },
+  {
+    "revision": "d2d82d68c7df91bb776f",
+    "url": "/react_tabs/static/js/2.e703e8cc.chunk.js"
+  },
+  {
+    "revision": "1b6d236442143cfa38d3",
+    "url": "/react_tabs/static/js/main.28f20e69.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
